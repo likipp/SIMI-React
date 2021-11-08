15da1e4b731c8caacf7bda9a7b1106f736ab939c
xiaom.liu <likipp@163.com>
2021-11-08 16:45:03 +0800

    完善应付单

--- src/pages/InList/index.tsx
+++ src/pages/InList/index.tsx
@@ -197,7 +197,7 @@ export default () => {
           </Button>
         ]}
         rowSelection={{
-          type: "checkbox",
+          type: 'radio',
           // 自定义选择项参考: https://ant.design/components/table-cn/#components-table-demo-row-selection-custom
           // 注释该行则默认不显示下拉选项
           // selections: [Table.SELECTION_ALL, Table.SELECTION_INVERT, Table.SELECTION_NONE,],
